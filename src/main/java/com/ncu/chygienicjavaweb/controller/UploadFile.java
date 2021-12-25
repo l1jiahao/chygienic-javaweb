cbef2a54ce5784af5229525ca34841ba3c7ff54b
nogralee <nogralee@gmail.com>
2021-12-25 20:38:43 +0800

    :fire: 添加上传文件的demo

---
+++ src/main/java/com/ncu/chygienicjavaweb/controller/UploadFile.java
@@ -0,0 +1,26 @@
+package com.ncu.chygienicjavaweb.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
+@RestController
+public class UploadFile {
+    @RequestMapping("/upload")
+    @ResponseBody
+    public String create(@RequestPart MultipartFile file) throws IOException {
+        String path = "/home/ubuntu/uploadFile/";
+        String fileName = file.getOriginalFilename();
+        String filePath = path + fileName;
+
+        File dest = new File(filePath);
+        Files.copy(file.getInputStream(), dest.toPath());
+        return "Upload file success : " + dest.getAbsolutePath();
+    }
+}
